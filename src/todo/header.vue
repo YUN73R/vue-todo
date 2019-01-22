0ceeeb3d1fcfcc28c3c4e1db5e3c662a8d0181fa
JXlinger <45476783+JXlinger@users.noreply.github.com>
2019-01-22 10:40:47 +0800

    Create header.vue

---
+++ src/todo/header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="headers">
+		todo vue入门小应用
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'headers',
+		data() {
+			return {
+
+			}
+		},
+	}
+</script>
+
+<style>
+	.headers{
+		position: fixed;
+		top: 0;
+		width: 750px;
+		line-height: 80px;
+		text-align: center;
+		background: #42B983;
+		color: #FFFFFF;
+		font-size: 20px;
+		font-weight: 600;
+	}
+</style>
