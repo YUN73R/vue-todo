3959c8ecd31f8df2c1b78053c598caab14c7307f
JXlinger <45476783+JXlinger@users.noreply.github.com>
2019-01-22 10:46:44 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="app">
+		<Header />
+		<router-view />
+	</div>
+</template>
+
+<script>
+	
+	import Header from './todo/header'
+	export default {
+		name: 'App',
+		components:{
+			Header
+			}
+	}
+</script>
+
+<style>
+	 @import url("style/globaal.css");
+</style>
